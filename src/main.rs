8cdb41d7e08b1353a083562b73b5e90e874d1d51
nithe14 <imnithe@gmail.com>
2022-09-12 11:11:42 +0200

    learning rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+#[macro_use]
+extern crate rocket;
+
+#[derive(Serialize, Deserialize)]
+struct BlogPost {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
+
+#[get("/random")]
+fn get_random_blog_post() -> Json<BlogPost> {
+    Json(BlogPost {
+        id: 1,
+        title: "My first post".to_string(),
+        body: "This is my first post".to_string(),
+        published: true,
+    })
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let rocket = rocket::build();
+    rocket
+        .mount("/", routes![index])
+        .mount("/", routes![get_random_blog_post])
 }
