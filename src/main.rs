379c72de41c09eef6b6ae66f69f2e1afca7ea2b0
nithe14 <imnithe@gmail.com>
2022-09-12 12:38:25 +0200

    learning rocket

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,8 @@ use std::io::Read;
 #[macro_use]
 extern crate rocket;
 
-#[derive(Serialize, Deserialize)]
-struct BlogPost {
-    id: i32,
-    title: String,
-    body: String,
-    published: bool,
-}
-
-#[get("/random")]
-fn get_random_blog_post() -> Json<BlogPost> {
-    Json(BlogPost {
-        id: 1,
-        title: "My first post".to_string(),
-        body: "This is my first post".to_string(),
-        published: true,
-    })
-}
-
-#[get("/")]
-fn index() -> std::string::String {
+#[get("/?<id>&<s>")]
+fn index(id: String, s: String) -> std::string::String {
     //&'static str {
     use rustc_serialize::json::Json;
     let mut file = File::open("text.json").unwrap();
@@ -32,14 +14,14 @@ fn index() -> std::string::String {
     file.read_to_string(&mut data).unwrap();
 
     let json = Json::from_str(&data).unwrap();
-    json.find_path(&["Title"]).unwrap().to_string()
+    //json.find_path(&["Title"]).unwrap().to_string()
+    let book = json.find_path(&[id.as_str()]).unwrap();
+    book.find_path(&[s.as_str()]).unwrap().to_string()
     //"Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build();
-    rocket
-        .mount("/", routes![index])
-        .mount("/", routes![get_random_blog_post])
+    rocket.mount("/", routes![index])
 }
