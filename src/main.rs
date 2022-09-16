3e14b01907555905f146269bd5943ca086e40f41
Nithe14 <imnithe@gmail.com>
2022-09-16 14:38:50 +0200

    add get_books_counter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rocket::serde::json::Json;
+use rustc_serialize::json::Json as rustcJson;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
@@ -11,26 +12,34 @@ struct Book {
     author: String,
 }
 
-#[get("/sentance?<id>&<s>")]
-fn get_sentance(id: String, s: String) -> std::string::String {
-    use rustc_serialize::json::Json;
+#[get("/books-counter")]
+fn get_books_count() -> String {
     let mut file = File::open("text.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
 
-    let json = Json::from_str(&data).unwrap();
+    let json = rustcJson::from_str(&data).unwrap();
+    json.as_array().unwrap().len().to_string()
+}
+
+#[get("/sentance?<id>&<s>")]
+fn get_sentance(id: String, s: String) -> std::string::String {
+    let mut file = File::open("text.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let json = rustcJson::from_str(&data).unwrap();
     let book = json.find_path(&[id.as_str()]).unwrap();
     book.find_path(&[s.as_str()]).unwrap().to_string()
 }
 
 #[get("/title?<id>")]
 fn get_title(id: String) -> Json<Book> {
-    use rustc_serialize::json::Json;
     let mut file = File::open("text.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
 
-    let json = Json::from_str(&data).unwrap();
+    let json = rustcJson::from_str(&data).unwrap();
     let book = json.find_path(&[id.as_str()]).unwrap();
 
     Json(Book {
@@ -47,5 +56,8 @@ fn index() -> String {
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build();
-    rocket.mount("/", routes![index, get_sentance, get_title])
+    rocket.mount(
+        "/",
+        routes![index, get_sentance, get_title, get_books_count],
+    )
 }
